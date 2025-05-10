07dff0cd706c3ce96c0db4e7828aaddba725683f
Sean Murphy <sean@iamseanmurphy.com>
2025-05-10 11:16:56 +0000

    2.0.2

--- wp-create-multiple-posts-pages/public/index.php
+++ wp-create-multiple-posts-pages/public/index.php
@@ -1,1 +1,8 @@
-<?php // Silence is golden
+phpcs -ps . --standard=WordPress --extensions=php<?php
+/**
+ * Silence is golden
+ *
+ * @since      2.0.0
+ * @package    Wp_Create_Multi_Posts_Pages
+ * @author     Sajjad Hossain Sagor <sagorh672@gmail.com>
+ */
